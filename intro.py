2856783ed5aea17b30024239b568223b0379c086
amyjprogrammer <amyjprogrammer@gmail.com>
2021-11-05 10:44:06 -0400

    first commit

---
+++ intro.py
@@ -0,0 +1,10 @@
+#single line of comment
+#EFA class to show people Python- which is an awesome language
+print("Hello")
+
+# """" or ''' for multi line comments
+
+x = 5 #assigning x the value 5
+result = x + 10
+print('result', result, 'x is', x)
+
